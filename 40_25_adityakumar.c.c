88301e0d4721cdce5cacfa0b6408534fc51d2114
Kaditya67 <140485509+Kaditya67@users.noreply.github.com>
2023-11-10 20:02:36 +0530

    40_25_adityakumar.c.c

--- 40_25_adityakumar.c.c
+++ 40_25_adityakumar.c.c
@@ -124,3 +124,70 @@ int main() {
 
     return 0;
 }
+
+// #include <stdio.h>
+// #include <stdbool.h>
+// #include <stdlib.h>
+
+// #define Vertices 100
+
+// struct Graph {
+//     int vertices;
+//     int matrix[Vertices][Vertices];
+// };
+
+// bool visited[Vertices];
+
+// struct Graph* createGraph(int vertices) {
+//     struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+//     graph->vertices = vertices;
+
+//     for (int i = 0; i < vertices; i++) {
+//         for (int j = 0; j < vertices; j++) {
+//             graph->matrix[i][j] = 0;
+//         }
+//     }
+//     return graph;
+// }
+
+// void addEdge(struct Graph* graph, int src, int dest) {
+//     graph->matrix[src][dest] = 1;
+//     graph->matrix[dest][src] = 1;
+// }
+
+// void BFS(struct Graph* graph, int startVertex) {
+//     int queue[Vertices]; // Use an array as a "circular queue"
+//     int front = 0, rear = 0;
+
+//     visited[startVertex] = true;
+//     printf("BFS Traversal starting from vertex %d: ", startVertex);
+
+//     queue[rear++] = startVertex;
+
+//     while (front < rear) {
+//         int currentVertex = queue[front++];
+
+//         printf("%d ", currentVertex);
+
+//         for (int i = 0; i < graph->vertices; i++) {
+//             if (graph->matrix[currentVertex][i] == 1 && !visited[i]) {
+//                 visited[i] = true;
+//                 queue[rear++] = i;
+//             }
+//         }
+//     }
+// }
+
+// int main() {
+//     struct Graph* graph = createGraph(5);
+
+//     addEdge(graph, 0, 1);
+//     addEdge(graph, 0, 2);
+//     addEdge(graph, 1, 3);
+//     addEdge(graph, 2, 4);
+
+//     int startVertex = 0;
+//     BFS(graph, startVertex);
+
+//     return 0;
+// }
